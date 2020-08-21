66c5e2bcdafb61e27bcc5f84cca0c1d4e0c242c3
Ihor <ruv1000@yandex.ru>
2020-08-21 12:45:11 +0300

    code improvement

--- calculator.py
+++ calculator.py
@@ -1,38 +1,42 @@
-# Первые шаги
-#Простейший калькулятор 
+# First steps
+# Simply calculator 
+
 from colorama import init
 from colorama import Fore, Back, Style
 init()
 
-what=input("Действие, которое будем выполнять(+, -, /, *): ")
+what=input("What arithmetic operations we will perform(+, -, /, *): ")
+
 if what == "+":
-	a=float(input("Первое число: "))
-	b=float(input("Второе число: "))
+	a=float(input("First number: "))
+	b=float(input("Second number: "))
 	c = a + b
-	print("Результат сложения: " + str(c))
+	print("Result: " + str(c))
 elif what == "-":
-	a=float(input("Первое число: "))
-	b=float(input("Второе число: "))
+	a=float(input("First number: "))
+	b=float(input("Second number: "))
 	c = a - b
-	print("Результат вычетания: " + str(c))
+	print("Result: " + str(c))
 elif what == "*":
-	a=float(input("Первое число: "))
-	b=float(input("Второе число: "))
+	a=float(input("First number: "))
+	b=float(input("Second number: "))
 	c = a * b
-	print("Результат умножения: " + str(c))
+	print("Result: " + str(c))
 elif what == "/":
-	a=float(input("Первое число: "))
-	b=float(input("Второе число: "))
+	a=float(input("First number: "))
+	b=float(input("Second number: "))
 	if b != 0:
 		r = a % b 
 		if r == 0:
 			c = a / b
-			print("Результат деления: " + str(c))
+			print("Result: " + str(c))
 		else:
 			c = a // b 
-			print("Результат деления: " + str(c) + " с остатком: " + str(r))
+			print("Result: " + str(c) + " remainder of the division: " + str(r))
 	else:
 		print(Back.RED)
-		print("Деление на ноль, ответа нет!")
+		print("Wrong! Division by zero, no answer!")
 else:
-	print("Данного арифметического знака не существует!")
+	print(Back.RED)
+	print("This arithmetic sign does not exist!")
+input()
