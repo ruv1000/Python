8fc32bf6f4863b5e771b6641cbd1d4a3d9cfe638
Ihor <ruv1000@yandex.ru>
2020-08-24 02:56:25 +0300

    optimized the code

--- WeatherTelegramBot.py
+++ WeatherTelegramBot.py
@@ -5,19 +5,23 @@ from pyowm import OWM
 bot = telebot.TeleBot("your-telegram-token")
 owm = OWM('your-API-key')
 
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Welcome")
+    bot.reply_to(message, "Welcome!" + "\n" + "Enter your city.")
+
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
-    city = message.text[0].upper() + message.text[1:] # Делаем первую букву введенного города заглавной
+    city = message.text[0].upper() + message.text[1:]  # Делаем первую букву введенного города заглавной
     mgr = owm.weather_manager()
     observation = mgr.weather_at_place(city)
     w = observation.weather
-	
-    answer ="It's " + str.lower(w.status) + " in " + city + " now." + "\n"
-    answer += "Average temperature " + str(round(w.temperature('celsius')["temp"]))+ ", feels like " + str(round(w.temperature('celsius')["feels_like"]))
-    bot.send_message(message.chat.id,answer)
+
+    answer = "It's " + str.lower(w.status) + " in " + city + " now." + "\n"
+    answer += "Average temperature " + str(round(w.temperature('celsius')["temp"])) + ", feels like " + str(
+        round(w.temperature('celsius')["feels_like"]))
+    bot.send_message(message.chat.id, answer)
+
 
 bot.polling(none_stop=True)
